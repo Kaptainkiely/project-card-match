960691bddd809fab21038ccb65d13d9105354d5a
Kaptainkiely <102674167+Kaptainkiely@users.noreply.github.com>
2023-02-12 15:59:24 +0000

    fixed same card click matching error.

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let lockboard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle('flip');
-  }
+    this.classList.add('flip');
+    if (this === firstCard) return;
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+      }
+
+      secondCard = this;
+   
+      checkForMatch();
+    }
+   
+    function checkForMatch() {
+      if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+      }
+   
+      unflipCards();
+    }
+   
+    function disableCards() {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+
+      resetBoard();
+    }
+   
+    function unflipCards() {
+        lockBoard = true;
+
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+      }, 1300);
+    }
+
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+      }
+  
   
   cards.forEach(card => card.addEventListener('click', flipCard));
