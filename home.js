863e6b6317fc64cd8b774afc4909c58c1d7e363e
Kaptainkiely <102674167+Kaptainkiely@users.noreply.github.com>
2023-03-13 20:41:19 +0000

    added function descriptions. sound on page load

--- home.js
+++
@@ -1,9 +0,0 @@
-const playButton = document.getElementById("play-button");
-playButton.addEventListener("click", playSound);
-
-
-
-function playSound() {
-    const audio = new Audio('sound/match.wav');
-    audio.play();
-  }
